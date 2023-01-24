cfe370f19a1bec7e12bc6d0f1bfcb1e44b6ef0cc
ari <aryfareq03@gmail.com>
2023-01-24 19:17:12 +0300

    debugged
    js has been debugged, but imma go ahead and add some more features

--- canvas.js
+++ canvas.js
@@ -1,5 +1,7 @@
-const canvas = document.querySelector('#draw');
-const ctx = canvas.getContex('2d');
+const canvas = document.querySelector('#Draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
@@ -8,21 +10,39 @@ ctx.lineWidth = '100';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
-function draw(e){
+function draw(e) {
     if(!isDrawing) return;
-    console.log(e);
-
+    console.log('working');
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
-
-    ctx.moveTo(lastx, lastY);
-
+    ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.strokeStyle();
+    ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
 
-}
-canvas.addEventListener('mouseMove', Draw);
-canvas.addEventListener('mouseDown', () => isDrawing = ture);
-canvas.addEventListener('mouseUp', () => isDrawing = false);
-canvas.addEventListener('mouseOut', () => isDrawing = false);
+    hue++;
+    if (hue >= 360) {
+      hue = 0;
+    }
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+      direction = !direction;
+    }
+  
+    if(direction) {
+      ctx.lineWidth++;
+    } else {
+      ctx.lineWidth--;
+    }
+    console.log('hello');
+  }
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
